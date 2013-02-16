53ec01f981a500e5823b1a6158c3cbefb2e0f9a3
charredUtensil <cjd.d01071@gmail.com>
2013-02-15 23:27:49 -0500

    dootdoot

--- stuff/meow.js
+++ stuff/meow.js
@@ -1,5 +1,4 @@
-
-if (Math.random() < 0.1) {
+//if (Math.random() < 0.1) {
 var babby=document.querySelectorAll("head")[0];
 babby.innerHTML = babby.innerHTML + "<style type='text/css'>.john.babby{position:fixed;left:-240px;bottom:0;-webkit-transition:all 7s ease-in;} .john.babby:hover{left:-100px;}</style>"
 babby=document.querySelectorAll("body")[0];
@@ -8,7 +7,8 @@ babby = true;
 booty = [
   "http://a.tumblr.com/tumblr_mia5xn7HFj1r8rjvxo1.mp3", //booty
   "http://a.tumblr.com/tumblr_meetn1vdA21rmvgizo1.mp3", //doctor
-  "http://a.tumblr.com/tumblr_mhg4z9l0TX1r1i0muo1.mp3"  //doctor2
+  "http://a.tumblr.com/tumblr_mhg4z9l0TX1r1i0muo1.mp3", //doctor2
+  "http://a.tumblr.com/tumblr_mia83hvK791qdqix8o1.mp3"  //dootdoot
 ];
 booty = new Audio(booty[Math.floor(Math.random()*booty.length)]);
-}
+//}

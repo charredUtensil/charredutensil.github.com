072143450e8158d4cd52a2e7377f48318564c3a5
charredUtensil <cjd.d01071@gmail.com>
2013-02-15 23:06:33 -0500

    more meow

--- stuff/meow.js
+++ stuff/meow.js
@@ -5,5 +5,10 @@ babby.innerHTML = babby.innerHTML + "<style type='text/css'>.john.babby{position
 babby=document.querySelectorAll("body")[0];
 babby.innerHTML = babby.innerHTML+'<img src="http://images1.wikia.nocookie.net/__cb20101230092213/mspaintadventures/images/2/2a/John_Godtier_-_Hood_down.png" class="john babby" onclick="if (babby) {babby = false; booty.play();}">';
 babby = true;
-booty = new Audio("http://a.tumblr.com/tumblr_mia5xn7HFj1r8rjvxo1.mp3");
+booty = [
+  "http://a.tumblr.com/tumblr_mia5xn7HFj1r8rjvxo1.mp3", //booty
+  "http://a.tumblr.com/tumblr_meetn1vdA21rmvgizo1.mp3", //doctor
+  "http://a.tumblr.com/tumblr_mhg4z9l0TX1r1i0muo1.mp3"  //doctor2
+];
+booty = new Audio(booty[Math.floor(Math.random()*booty.length)]);
 }

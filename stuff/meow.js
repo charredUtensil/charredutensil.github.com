89d90ab60388f72c5732e0d7d24bf0c9b0e354e5
charredUtensil <cjd.d01071@gmail.com>
2013-02-15 22:48:37 -0500

    add meow

---
+++ stuff/meow.js
@@ -0,0 +1,9 @@
+
+if (Math.random() < 0.1) {
+var babby=document.querySelectorAll("head")[0];
+babby.innerHTML = babby.innerHTML + "<style type='text/css'>.john.babby{position:fixed;left:-240px;bottom:0;-webkit-transition:all 7s ease-in;} .john.babby:hover{left:-100px;}</style>"
+babby=document.querySelectorAll("body")[0];
+babby.innerHTML = babby.innerHTML+'<img src="http://images1.wikia.nocookie.net/__cb20101230092213/mspaintadventures/images/2/2a/John_Godtier_-_Hood_down.png" class="john babby" onclick="if (babby) {babby = false; booty.play();}">';
+babby = true;
+booty = new Audio("http://a.tumblr.com/tumblr_mia5xn7HFj1r8rjvxo1.mp3");
+}

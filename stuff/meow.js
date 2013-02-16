35b84e380e3abea1adc307f10300abce13dc4145
charredUtensil <cjd.d01071@gmail.com>
2013-02-15 23:42:30 -0500

    why hello thar

--- stuff/meow.js
+++ stuff/meow.js
@@ -1,6 +1,10 @@
 //if (Math.random() < 0.1) {
 var babby=document.querySelectorAll("head")[0];
-babby.innerHTML = babby.innerHTML + "<style type='text/css'>.john.babby{position:fixed;left:-240px;bottom:0;-webkit-transition:all 7s ease-in;} .john.babby:hover{left:-100px;}</style>"
+booty = [
+  "<style type='text/css'>.john.babby{position:fixed;left:-240px;bottom:0;-webkit-transition:all 7s ease-in;} .john.babby:hover{left:-100px;}</style>",
+  "<style type='text/css'>.john.babby{position: fixed;left: 120px;top: -210px;-webkit-transform: rotate(180deg);-webkit-transition: all 1s ease-in-out;} .john.babby:hover{top: -90px;-webkit-transform: rotate(160deg);}</style>"
+];
+babby.innerHTML = babby.innerHTML + booty[Math.floor(Math.random()*booty.length)]
 babby=document.querySelectorAll("body")[0];
 babby.innerHTML = babby.innerHTML+'<img src="http://images1.wikia.nocookie.net/__cb20101230092213/mspaintadventures/images/2/2a/John_Godtier_-_Hood_down.png" class="john babby" onclick="if (babby) {babby = false; booty.play();}">';
 babby = true;
